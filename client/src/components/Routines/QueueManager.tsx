db08bc8fc1bdc111aee0849872dceab13a4dc1a5
Senshiben-efrei <ayman.ben.hajjaj@efrei.net>
2024-12-09 20:07:20 +0100

    fix itteration addin

--- client/src/components/Routines/QueueManager.tsx
+++ client/src/components/Routines/QueueManager.tsx
@@ -16,6 +16,14 @@ const generateUUID = () => {
   });
 };
 
+const generateUUID = () => {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    const r = Math.random() * 16 | 0;
+    const v = c === 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+};
+
 interface QueueManagerProps {
   queue: Queue;
   areas: Area[];
