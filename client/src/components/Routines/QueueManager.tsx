d5739c2921a1b7bcef7152da49557d854645b8db
Senshiben-efrei <ayman.ben.hajjaj@efrei.net>
2024-12-09 20:07:20 +0100

    fix itteration addin

--- client/src/components/Routines/QueueManager.tsx
+++ client/src/components/Routines/QueueManager.tsx
@@ -8,6 +8,14 @@ import CooldownForm from './CooldownForm';
 import { DocumentDuplicateIcon } from '@heroicons/react/24/outline';
 import { generateUUID } from '../../utils/uuid';
 
+const generateUUID = () => {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    const r = Math.random() * 16 | 0;
+    const v = c === 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+};
+
 interface QueueManagerProps {
   queue: Queue;
   areas: Area[];
